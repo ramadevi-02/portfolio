704033f43cd6aeb991bcab210d99dcb48e6c86cd
ramadevi-02 <ramadevi.unnam02@gmail.com>
2024-09-24 18:21:16 +0530

    Yolo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import yolo from "../assets/img/yolo.jpg";
+import web from "../assets/img/web.jpg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -12,7 +12,7 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={yolo} alt="Logo" />
+            <img src={web} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
